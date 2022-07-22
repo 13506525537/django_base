73fbdb3e4d913ed0bcb47d71c57b5a2266477a54
tofflon <liyan06011@tofflon.com>
2022-07-22 11:31:41 +0800

    定义模型

--- Helloworld/book/models.py
+++ Helloworld/book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+# models使用ORM模式和数据库交互，不直接操作数据库
 # Create your models here.
+
+# 一个类对应一个表
+class Book(models.Model):
+    # Book有两个属性id name
+    name = models.CharField(max_length=10)  # 书名字段
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)  # 人物名字
+    gender = models.BooleanField() # 人物性别
+
+    # 外链的表
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
