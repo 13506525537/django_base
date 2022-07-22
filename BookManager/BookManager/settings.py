515424a6cd7a0450c71f53b57558b83d8471593b
tofflon <liyan06011@tofflon.com>
2022-07-22 17:08:12 +0800

    创建数据库

--- BookManager/BookManager/settings.py
+++ BookManager/BookManager/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-89aub+!bt*%y!=mp(b0x0_52tl5+2f$t!vf=$um%8)7@r^^&cx
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BookManager.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '192.168.17.128',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'Qwert1234567',
+        'NAME': 'book',  # 数据库名
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -111,7 +110,6 @@ TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- BookManager/book/models.py
+++ BookManager/book/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Book(models.Model):
-    pass
+# 一个类对应一个表
+# 表名默认为：子应用名_类名  都是小写
+class BookInfo(models.Model):
+    # id 默认
+    # 属性等于字段
+    name = models.CharField(max_length=32, unique=True)  # 书名
+    pub_date = models.DateField(null=True)  # 出版日期
+    readcount = models.IntegerField()  # 阅读数
+    commentcount = models.IntegerField(max_length=10)  # 评论数
+    is_delete = models.BooleanField(default=False)  # 是否删除
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'bookinfo'  # 修改表的名字
+
+
+class PersonInfo(models.Model):
+    # id 默认
+    # 创建字段
+    name = models.CharField(max_length=32)
+    gender = models.IntegerField()
