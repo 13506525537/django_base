b6832f150ac8332dc87a78b856c6f084e08f433e
liyan <1003955517@qq.com>
2022-07-25 20:07:15 +0800

    Httpresponse函数

--- bookmanager02/book/urls.py
+++ bookmanager02/book/urls.py
@@ -1,13 +1,16 @@
 """
 app 路由表
 """
-from book.views import index, create_book, shop, class_no,register
+from book.views import index, create_book, shop, class_no, register, response
 from django.urls import path
+
+from django.urls import converters  # 转化器模块，里面可以自己添加
 
 urlpatterns = [
     path('index/', index),
     path('create/', create_book),  # 获取请求中路径的参数*****
-    path('shop/<city_id>/<shop_id>', shop),  # 获取请求中路径的参数*****
+    path('shop/<int:city_id>/<int:shop_id>', shop),  # <转化器名称:变量名> 转换器会对变量进行正则验证
     path('school/<class_no>', class_no),
-    path('register/', register)
+    path('register/', register),
+    path('res/', response)
 ]
--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -31,7 +31,7 @@ def shop(request, city_id, shop_id):
     # 使用getlist获取一键多值的值
     order = query_params.getlist('keyword')
     print(order)
-    return HttpResponse(order)
+    return HttpResponse(f'{city_id}的{shop_id}存在！')
 
 def class_no(request,class_no):
     query_params = request.GET # 获取name的参数
@@ -49,3 +49,13 @@ def register(request):
 
     return render(request,template_name='register.html')
     # return HttpResponse('ok')
+
+def response(request): # 测试响应
+    response = HttpResponse('res',status=200) # 第一个参数为响应体，第二个为状态码
+    response['name']='itcast' #可以通过这种方式这只响应头
+    return response
+    # 1XX 消息
+    # 2XX 成功
+    # 3XX 重定向
+    # 4XX 客户端 404路由有问题
+    # 5XX 服务器
